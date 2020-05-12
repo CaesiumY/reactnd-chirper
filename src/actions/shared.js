f474afbb55ed019fc72ef640996dd0cb7a6c10fd
caesiumy <mn0658@naver.com>
2020-05-12 22:34:24 +0900

    feat: Add authedUser action

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
 import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweets";
 import { receiveUser } from "./users";
+import { setAuthedUser } from "./authedUser";
+
+const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUser(users));
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }
