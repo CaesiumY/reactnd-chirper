ac18fba7625e73f76033157bbce9f487bb36573c
caesiumy <mn0658@naver.com>
2020-05-12 23:30:27 +0900

    feat: Add reducers

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,8 @@
-export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_USERS = "RECEIVE_USERS";
 
 export function receiveUser(users) {
   return {
-    type: RECEIVE_USER,
+    type: RECEIVE_USERS,
     users,
   };
 }
