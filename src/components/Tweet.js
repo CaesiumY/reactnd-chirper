de2efc73b31f8d245eeed47c955eb01cc15b8708
caesiumy <mn0658@naver.com>
2020-05-13 21:51:32 +0900

    feat: Update mapStateToProps in Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,16 +4,25 @@ import { formatTweet } from "../utils/helpers";
 
 export class Tweet extends Component {
   render() {
+    const { tweet } = this.props;
     console.log(this.props);
+
+    if (tweet === null) {
+      return <p>This tweet does not exist.</p>;
+    }
+
     return <div className="tweet"></div>;
   }
 }
 
 const mapStateToProps = ({ tweets, users, authedUser }, { id }) => {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
   return {
     users,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 };
 
