dbbda90680c26489e37ab3c0aea6942cfe421728
caesiumy <mn0658@naver.com>
2020-05-14 19:11:52 +0900

    feat: Add router path in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import LoadingBar from "react-redux-loading-bar";
+import { HashRouter, Route } from "react-router-dom";
 import { handleInitialData } from "../actions/shared";
-// import Dashboard from "./Dashboard";
-// import NewTweet from "./NewTweet";
+import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
 import TweetPage from "./TweetPage";
 
 class App extends Component {
@@ -12,14 +13,22 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading ? (
-          <h3 className="center">Loading...</h3>
-        ) : (
-          <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
-        )}
-      </div>
+      <HashRouter>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            {this.props.loading ? (
+              <h3 className="center">Loading...</h3>
+            ) : (
+              <div>
+                <Route path="/" exact component={Dashboard}></Route>
+                <Route path="/tweet/:id" component={TweetPage}></Route>
+                <Route path="/new" component={NewTweet}></Route>
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </HashRouter>
     );
   }
 }
